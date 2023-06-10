7c42529bf554bebc69a2539d2b881642ffc65d9e
Judy Ye <judyy.ye@gmail.com>
2023-06-09 23:02:40 -0700

    Refactor to DRY up code

--- main.js
+++ main.js
@@ -4,6 +4,17 @@ var playerOne;
 var playerTwo;
 var currentPlayer;
 
+var winningCombos = [
+  [1, 2, 3], 
+  [4, 5, 6], 
+  [7, 8, 9], 
+  [1, 4, 7], 
+  [2, 5, 8], 
+  [3, 6, 9],
+  [3, 5, 7], 
+  [1, 5, 9]
+]
+
 // querySelectors
 // boxOne = document.querySelector('#one')
 // boxTwo = document.querySelector('#two')
@@ -11,6 +22,7 @@ var currentPlayer;
 // boxFour = document.querySelector('#four')
 boxes = document.querySelectorAll(".box");
 boardHeader = document.querySelector('.play-section-header')
+gridBoard = document.querySelector('.grid-board')
 
 // eventListeners
 // boxOne.addEventListener('click', addSymbol)
@@ -23,6 +35,7 @@ for (var i = 0; i < boxes.length; i++) {
     keepTrack(e);
   }, {once: true});
 }
+
 
 // idk what these are
   // after page shows a payer won:
@@ -40,7 +53,7 @@ function storePlayerInfo(id, token) {
   return {
     id: id,
     token: token, 
-    isPlayerTurn: true,
+    // isPlayerTurn: true,
     wins: 0,
     boxTargets: []
   };
@@ -48,8 +61,10 @@ function storePlayerInfo(id, token) {
 
 function startGame() {
   createPlayer();
+  displayBoard();
   currentPlayer = playerOne
   displayPlayer(); 
+  // checkWins();
 }
 
 // updates Data Model:
@@ -66,59 +81,46 @@ function createPlayer() {
   // eventListener on boxes, click, hide player[i].isPlayerTurn or remove
   // make isPlayerTurn false
 function displayPlayer() {
-  console.log('showing player')
-  //check which player goes
-  if (currentPlayer === playerOne) {
-    boardHeader.innerHTML = `<h3 class="player-one-head">Hey ${playerOne.token}, it's your turn!</h3>`
-    // after click, go to other player
-    //boardHeader.firstElementChild.classList.remove('player-two-head')
-  } 
-  // how to update DOM?
-  if (currentPlayer === playerTwo) {
-    boardHeader.innerHTML = `<h3 class="player-two-head">Hey ${playerTwo.token}, it's your turn!<h3>`
-  }
-  //togglePlayer()
-  // can't use keepTrack it throws an undefined for target
+  boardHeader.innerHTML = `<h3 class="player-one-head">Hey ${currentPlayer.token}, it's your turn!</h3>`
 }
-
-// function togglePlayer() {
-//   // for (var i = 0; i < allPlayers.length; i++) {
-//   //   // if currentPlayer clicked make isPlayerTurn false
-//   //   allPlayers[i].isPlayerTurn = !allPlayers[i].isPlayerTurn
-//   // }
-//   playerOne.isPlayerTurn = !playerOne.isPlayerTurn
-// }
 
 function keepTrack(e) {
   if (currentPlayer === playerOne) {
     e.target.innerHTML += `<div class="player-one-emoji">🥔</div>`;
     playerOne.boxTargets.push(parseInt(e.target.getAttribute('id')));
-    // change player
-    //playerOne.isPlayerTurn = false
-    // //console.log('whose turn:', !playerOne.isPlayerTurn)
-    //playerTwo.isPlayerTurn = true
-    // //console.log('P2:', playerTwo.isPlayerTurn)
   }
   if (currentPlayer === playerTwo) {
     e.target.innerHTML += `<div class="player-two-emoji">🍟</div>`;
     playerTwo.boxTargets.push(parseInt(e.target.getAttribute('id')));
-    // playerTwo.isPlayerTurn = false
-    // playerOne.isPlayerTurn = true
   }
   togglePlayer()
 }
 
-// make Toggle do: isPlayerTurn = !isPlayerTurn
-// isPlayerTurn ? true : !isPlayerTurn
 function togglePlayer() {
-  //if currentPlayer is playerOne, make it playerTwo
   currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne
+  displayPlayer()
 }
+
+// function checkWins() {
+//   console.log('potatooo')
+//   for (var i = 0; i < winningCombos.length; i++) {
+//     for (var j = 0; j < winningCombos[i].length; j++) {
+//       for (var k = 0; k < allPlayers.length; k++) {       
+//         if (allPlayers[k].boxTargets.includes(winningCombos[i][j])) {
+//           increaseWins()
+//         }
+//       }
+//     }
+//   }
+// }
+
+
 
 function increaseWins() {
   for (var i = 0; i < allPlayers.length; i++) {
     allPlayers[i].wins++;
   }
+  storePlayerInfo()
 }
 // update DOM
 
